da9441122e7f95540e37df308f7c6bfddcc611eb
username <maneikebmx@icloud.com>
2021-04-01 23:05:25 +0200

    Hid navigation header

--- App.js
+++ App.js
@@ -11,7 +11,10 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
         <Stack.Screen name="First Screen" component={FirstScreen} />
         <Stack.Screen name="Second Screen" component={SecondScreen} />
       </Stack.Navigator>
