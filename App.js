17d963c64e453cc0b2f5484ebc7a1987cf7f9a50
username <maneikebmx@icloud.com>
2021-04-01 13:27:14 +0200

    Added fetch and its thumbnail and name display

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {
+  Button,
+  Image,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -17,15 +19,44 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import useFetch from './helpers/useFetch';
+
 function App() {
+  const {data, loading} = useFetch('https://api.randomuser.me');
   return (
     <SafeAreaView>
-      <Text>Init</Text>
-      <Text>Commit test</Text>
+      <View styles={styles.container}>
+        <View style="styles.userInfo">
+          {!data && loading ? (
+            <Text>...loading</Text>
+          ) : (
+            <View>
+              <Image
+                style={{width: 50, height: 50, borderRadius: 37.5}}
+                source={{uri: data.picture.thumbnail}}
+              />
+              <Text>
+                {data.name.first} {data.name.last}
+              </Text>
+            </View>
+          )}
+        </View>
+        <View>
+          <Button title="hello"></Button>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  userInfo: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
 
 export default App;
---
+++ helpers/useFetch.js
@@ -0,0 +1,15 @@
+import {useEffect, useState} from 'react';
+const useFetch = url => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const [item] = data.results;
+    setData(item);
+    setLoading(false);
+  }, []);
+  return {data, loading};
+};
+export default useFetch;
