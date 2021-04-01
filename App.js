e56d533913bf724bd2286b27a63b3b1c982c0343
username <maneikebmx@icloud.com>
2021-04-01 12:50:02 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>Init</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
